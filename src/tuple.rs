ad910dd734b41d5cb17a1357cee685090413c2b1
Wei Tang <hi@that.world>
2019-06-30 10:24:30 +0200

    Should use empty value from db

--- src/tuple.rs
+++ src/tuple.rs
@@ -107,7 +107,8 @@ impl<R: RootStatus, DB: MerkleDB> MerkleTuple<R, DB> {
             Some(value) => value.end().ok_or(Error::CorruptedDatabase)?,
             None => return Err(Error::CorruptedDatabase),
         };
-        self.raw.set(db, raw_index, Value::End(Default::default()))?;
+        let empty = db.empty_at(0)?;
+        self.raw.set(db, raw_index, empty)?;
 
         if len <= self.max_len() / 2 {
             self.shrink(db)?;
