d502a35adc3fb9de8b43daeca5337ca6ffa40c53
Wei Tang <hi@that.world>
2019-07-06 21:04:47 +0200

    Generalize SerializeVector

--- ssz/src/fixed.rs
+++ ssz/src/fixed.rs
@@ -4,14 +4,18 @@ use primitive_types::{U256, H256};
 
 use crate::{Serial, Intermediate, End, Composite};
 
+pub trait SerializeVector<DB: Backend> {
+    fn serialize_vector(&self, db: &mut DB, at_depth: Option<usize>) -> Result<ValueOf<DB>, Error<DB::Error>>;
+}
+
 pub struct FixedVec<T>(pub Vec<T>);
 
 macro_rules! impl_builtin_fixed_uint_vector {
     ( $( $t:ty ),* ) => { $(
-        impl<'a, DB> Serialize<DB> for Serial<'a, FixedVec<$t>> where
+        impl<'a, DB> SerializeVector<DB> for Serial<'a, FixedVec<$t>> where
             DB: Backend<Intermediate=Intermediate, End=End>
         {
-            fn serialize(&self, db: &mut DB) -> Result<ValueOf<DB>, Error<DB::Error>> {
+            fn serialize_vector(&self, db: &mut DB, at_depth: Option<usize>) -> Result<ValueOf<DB>, Error<DB::Error>> {
                 let mut chunks: Vec<Vec<u8>> = Vec::new();
 
                 for value in &(self.0).0 {
@@ -27,7 +31,7 @@ macro_rules! impl_builtin_fixed_uint_vector {
                     let mut ret = End::default();
                     ret.0.copy_from_slice(&c);
                     Value::End(ret)
-                }).collect::<Vec<_>>(), db, None)
+                }).collect::<Vec<_>>(), db, at_depth)
             }
         }
     )* }
@@ -35,33 +39,42 @@ macro_rules! impl_builtin_fixed_uint_vector {
 
 impl_builtin_fixed_uint_vector!(u8, u16, u32, u64, u128);
 
-impl<'a, DB> Serialize<DB> for Serial<'a, FixedVec<U256>> where
+impl<'a, DB> SerializeVector<DB> for Serial<'a, FixedVec<U256>> where
     DB: Backend<Intermediate=Intermediate, End=End>
 {
-    fn serialize(&self, db: &mut DB) -> Result<ValueOf<DB>, Error<DB::Error>> {
+    fn serialize_vector(&self, db: &mut DB, at_depth: Option<usize>) -> Result<ValueOf<DB>, Error<DB::Error>> {
         serialize::serialize_vector(&(self.0).0.iter().map(|uint| {
             let mut ret = End::default();
             uint.to_little_endian(&mut ret.0);
             Value::End(ret)
-        }).collect::<Vec<_>>(), db, None)
+        }).collect::<Vec<_>>(), db, at_depth)
     }
 }
 
-impl<'a, DB> Serialize<DB> for Serial<'a, H256> where
+impl<'a, DB> SerializeVector<DB> for Serial<'a, H256> where
     DB: Backend<Intermediate=Intermediate, End=End>
 {
-    fn serialize(&self, db: &mut DB) -> Result<ValueOf<DB>, Error<DB::Error>> {
-        Serial(&FixedVec(self.0.as_ref().iter().cloned().collect())).serialize(db)
+    fn serialize_vector(&self, db: &mut DB, at_depth: Option<usize>) -> Result<ValueOf<DB>, Error<DB::Error>> {
+        Serial(&FixedVec(self.0.as_ref().iter().cloned().collect())).serialize_vector(db, at_depth)
     }
 }
 
-impl<'a, T: Composite, DB> Serialize<DB> for Serial<'a, FixedVec<T>> where
+impl<'a, T: Composite, DB> SerializeVector<DB> for Serial<'a, FixedVec<T>> where
     for<'b> Serial<'b, T>: Serialize<DB>,
     DB: Backend<Intermediate=Intermediate, End=End>
 {
-    fn serialize(&self, db: &mut DB) -> Result<ValueOf<DB>, Error<DB::Error>> {
+    fn serialize_vector(&self, db: &mut DB, at_depth: Option<usize>) -> Result<ValueOf<DB>, Error<DB::Error>> {
         serialize::serialize_vector(&(self.0).0.iter().map(|value| {
             Serial(value).serialize(db)
-        }).collect::<Result<Vec<_>, _>>()?, db, None)
+        }).collect::<Result<Vec<_>, _>>()?, db, at_depth)
+    }
+}
+
+impl<'a, DB, T> Serialize<DB> for Serial<'a, FixedVec<T>> where
+    DB: Backend<Intermediate=Intermediate, End=End>,
+    Self: SerializeVector<DB>,
+{
+    fn serialize(&self, db: &mut DB) -> Result<ValueOf<DB>, Error<DB::Error>> {
+        self.serialize_vector(db, None)
     }
 }
