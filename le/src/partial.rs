0193f26704cc907ac619d8f4be9cb195de6ea85a
Wei Tang <hi@that.world>
2019-08-30 23:20:06 +0200

    Tabify all sources and add editorconfig

--- le/src/partial.rs
+++ le/src/partial.rs
@@ -10,249 +10,249 @@ use crate::{FromTree, IntoTree, CompatibleConstruct};
 /// Partial index for le binary tree.
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct PartialIndex {
-    parent: Option<Box<PartialIndex>>,
-    sub: PartialSubIndex,
+	parent: Option<Box<PartialIndex>>,
+	sub: PartialSubIndex,
 }
 
 /// Partial sub-index for le binary tree.
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum PartialSubIndex {
-    /// Static index.
-    Raw(Index),
-    /// Index that is dynamic to a length.
-    List(usize),
-    /// Index from a vector.
-    Vector(usize, usize),
+	/// Static index.
+	Raw(Index),
+	/// Index that is dynamic to a length.
+	List(usize),
+	/// Index from a vector.
+	Vector(usize, usize),
 }
 
 impl PartialIndex {
-    /// Partial index from root.
-    pub fn root() -> Self {
-        Self {
-            parent: None,
-            sub: PartialSubIndex::Raw(Index::root()),
-        }
-    }
+	/// Partial index from root.
+	pub fn root() -> Self {
+		Self {
+			parent: None,
+			sub: PartialSubIndex::Raw(Index::root()),
+		}
+	}
 
-    /// Partial index to a raw item.
-    pub fn raw(&self, index: Index) -> Self {
-        Self {
-            parent: Some(Box::new(self.clone())),
-            sub: PartialSubIndex::Raw(index),
-        }
-    }
+	/// Partial index to a raw item.
+	pub fn raw(&self, index: Index) -> Self {
+		Self {
+			parent: Some(Box::new(self.clone())),
+			sub: PartialSubIndex::Raw(index),
+		}
+	}
 
-    /// Partial index to a vector.
-    pub fn vector(&self, index: usize, len: usize) -> Self {
-        Self {
-            parent: Some(Box::new(self.clone())),
-            sub: PartialSubIndex::Vector(index, len),
-        }
-    }
+	/// Partial index to a vector.
+	pub fn vector(&self, index: usize, len: usize) -> Self {
+		Self {
+			parent: Some(Box::new(self.clone())),
+			sub: PartialSubIndex::Vector(index, len),
+		}
+	}
 
-    /// Partial index to a list.
-    pub fn list(&self, index: usize) -> Self {
-        Self {
-            parent: Some(Box::new(self.clone())),
-            sub: PartialSubIndex::List(index),
-        }
-    }
+	/// Partial index to a list.
+	pub fn list(&self, index: usize) -> Self {
+		Self {
+			parent: Some(Box::new(self.clone())),
+			sub: PartialSubIndex::List(index),
+		}
+	}
 
-    /// Resolve the index.
-    pub fn resolve<R: RootStatus, DB: ReadBackend>(
-        &self,
-        raw: &Raw<R, DB::Construct>,
-        db: &mut DB
-    ) -> Result<Index, Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct,
-    {
-        let parent = match &self.parent {
-            Some(p) => p.resolve(raw, db)?,
-            None => Index::root(),
-        };
+	/// Resolve the index.
+	pub fn resolve<R: RootStatus, DB: ReadBackend>(
+		&self,
+		raw: &Raw<R, DB::Construct>,
+		db: &mut DB
+	) -> Result<Index, Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct,
+	{
+		let parent = match &self.parent {
+			Some(p) => p.resolve(raw, db)?,
+			None => Index::root(),
+		};
 
-        let (index, len) = match self.sub {
-            PartialSubIndex::Raw(raw) => return Ok(parent.sub(raw)),
-            PartialSubIndex::Vector(index, len) => (index, len),
-            PartialSubIndex::List(index) => {
-                let len_root = raw.get(db, parent.right())?.ok_or(Error::CorruptedDatabase)?;
-                let len = U256::from_tree(&len_root, db)?;
+		let (index, len) = match self.sub {
+			PartialSubIndex::Raw(raw) => return Ok(parent.sub(raw)),
+			PartialSubIndex::Vector(index, len) => (index, len),
+			PartialSubIndex::List(index) => {
+				let len_root = raw.get(db, parent.right())?.ok_or(Error::CorruptedDatabase)?;
+				let len = U256::from_tree(&len_root, db)?;
 
-                if len > U256::from(usize::max_value()) {
-                    return Err(Error::CorruptedDatabase)
-                } else {
-                    (index, len.as_usize())
-                }
-            },
-        };
+				if len > U256::from(usize::max_value()) {
+					return Err(Error::CorruptedDatabase)
+				} else {
+					(index, len.as_usize())
+				}
+			},
+		};
 
-        let mut max_len = 1;
-        let mut depth = 0;
-        while max_len < len {
-            max_len *= 2;
-            depth += 1;
-        }
-        let sub = Index::from_one((1 << depth) + index).expect("
-          result is greater or equal to 1;
-          Index::from_one always return Some; qed");
+		let mut max_len = 1;
+		let mut depth = 0;
+		while max_len < len {
+			max_len *= 2;
+			depth += 1;
+		}
+		let sub = Index::from_one((1 << depth) + index).expect("
+		  result is greater or equal to 1;
+		  Index::from_one always return Some; qed");
 
-        Ok(parent.sub(sub))
-    }
+		Ok(parent.sub(sub))
+	}
 }
 
 /// Basic partial values.
 pub struct PartialValue<T> {
-    index: PartialIndex,
-    value: Option<T>,
+	index: PartialIndex,
+	value: Option<T>,
 }
 
 impl<T: FromTree> PartialValue<T> {
-    /// Fetch the partial value from the database.
-    pub fn fetch<R: RootStatus, DB: ReadBackend>(
-        &mut self,
-        raw: &Raw<R, DB::Construct>,
-        db: &mut DB,
-    ) -> Result<(), Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct
-    {
-        let index = self.index.resolve(raw, db)?;
-        let index_root = raw.get(db, index)?.ok_or(Error::CorruptedDatabase)?;
-        let value = T::from_tree(&index_root, db)?;
+	/// Fetch the partial value from the database.
+	pub fn fetch<R: RootStatus, DB: ReadBackend>(
+		&mut self,
+		raw: &Raw<R, DB::Construct>,
+		db: &mut DB,
+	) -> Result<(), Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct
+	{
+		let index = self.index.resolve(raw, db)?;
+		let index_root = raw.get(db, index)?.ok_or(Error::CorruptedDatabase)?;
+		let value = T::from_tree(&index_root, db)?;
 
-        self.value = Some(value);
-        Ok(())
-    }
+		self.value = Some(value);
+		Ok(())
+	}
 
-    /// Get a reference to the fetched partial value.
-    pub fn get<R: RootStatus, DB: ReadBackend>(
-        &mut self,
-        raw: &Raw<R, DB::Construct>,
-        db: &mut DB,
-    ) -> Result<&T, Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct
-    {
-        if self.value.is_none() {
-            self.fetch(raw, db)?;
-        }
+	/// Get a reference to the fetched partial value.
+	pub fn get<R: RootStatus, DB: ReadBackend>(
+		&mut self,
+		raw: &Raw<R, DB::Construct>,
+		db: &mut DB,
+	) -> Result<&T, Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct
+	{
+		if self.value.is_none() {
+			self.fetch(raw, db)?;
+		}
 
-        Ok(self.value.as_ref().expect("value is checked to be some or set before; qed"))
-    }
+		Ok(self.value.as_ref().expect("value is checked to be some or set before; qed"))
+	}
 
-    /// Set the partial value.
-    pub fn set(&mut self, value: T) {
-        self.value = Some(value);
-    }
+	/// Set the partial value.
+	pub fn set(&mut self, value: T) {
+		self.value = Some(value);
+	}
 }
 
 impl<T: IntoTree> PartialItem for PartialValue<T> {
-    fn new(index: PartialIndex) -> Self {
-        Self {
-            index,
-            value: None
-        }
-    }
+	fn new(index: PartialIndex) -> Self {
+		Self {
+			index,
+			value: None
+		}
+	}
 
-    fn flush<R: RootStatus, DB: WriteBackend>(
-        &mut self,
-        raw: &mut Raw<R, DB::Construct>,
-        db: &mut DB,
-    ) -> Result<(), Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct
-    {
-        if let Some(value) = self.value.take() {
-            let index = self.index.resolve(raw, db)?;
-            let value_root = value.into_tree(db)?;
+	fn flush<R: RootStatus, DB: WriteBackend>(
+		&mut self,
+		raw: &mut Raw<R, DB::Construct>,
+		db: &mut DB,
+	) -> Result<(), Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct
+	{
+		if let Some(value) = self.value.take() {
+			let index = self.index.resolve(raw, db)?;
+			let value_root = value.into_tree(db)?;
 
-            raw.set(db, index, value_root)?;
-        }
+			raw.set(db, index, value_root)?;
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 /// Partial item for Vec.
 pub struct PartialVec<T: Partialable> {
-    index: PartialIndex,
-    values: Map<usize, T::Value>,
-    pushed: Vec<T>,
+	index: PartialIndex,
+	values: Map<usize, T::Value>,
+	pushed: Vec<T>,
 }
 
 impl<T: Partialable> PartialVec<T> {
-    /// Access a value at given position.
-    pub fn at(&mut self, index: usize) -> &mut T::Value {
-        self.values.entry(index).or_insert(PartialItem::new(PartialIndex {
-            parent: Some(Box::new(self.index.clone())),
-            sub: PartialSubIndex::List(index),
-        }))
-    }
+	/// Access a value at given position.
+	pub fn at(&mut self, index: usize) -> &mut T::Value {
+		self.values.entry(index).or_insert(PartialItem::new(PartialIndex {
+			parent: Some(Box::new(self.index.clone())),
+			sub: PartialSubIndex::List(index),
+		}))
+	}
 
-    /// Push a value at given position.
-    pub fn push(&mut self, value: T) {
-        self.pushed.push(value);
-    }
+	/// Push a value at given position.
+	pub fn push(&mut self, value: T) {
+		self.pushed.push(value);
+	}
 }
 
 impl<T: Partialable + IntoTree> PartialItem for PartialVec<T> {
-    fn new(index: PartialIndex) -> Self {
-        Self {
-            index,
-            values: Default::default(),
-            pushed: Default::default(),
-        }
-    }
+	fn new(index: PartialIndex) -> Self {
+		Self {
+			index,
+			values: Default::default(),
+			pushed: Default::default(),
+		}
+	}
 
-    fn flush<R: RootStatus, DB: WriteBackend>(
-        &mut self,
-        raw: &mut Raw<R, DB::Construct>,
-        db: &mut DB,
-    ) -> Result<(), Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct,
-    {
-        let mut values = Map::default();
-        mem::swap(&mut values, &mut self.values);
+	fn flush<R: RootStatus, DB: WriteBackend>(
+		&mut self,
+		raw: &mut Raw<R, DB::Construct>,
+		db: &mut DB,
+	) -> Result<(), Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct,
+	{
+		let mut values = Map::default();
+		mem::swap(&mut values, &mut self.values);
 
-        for (_, mut value) in values {
-            value.flush(raw, db)?;
-        }
+		for (_, mut value) in values {
+			value.flush(raw, db)?;
+		}
 
-        let mut pushed = Vec::default();
-        mem::swap(&mut pushed, &mut self.pushed);
-        let mut list = DanglingList::reconstruct(raw.root(), db, None)?;
-        for value in pushed {
-            let value_root = value.into_tree(db)?;
-            list.push(db, value_root)?;
-        }
+		let mut pushed = Vec::default();
+		mem::swap(&mut pushed, &mut self.pushed);
+		let mut list = DanglingList::reconstruct(raw.root(), db, None)?;
+		for value in pushed {
+			let value_root = value.into_tree(db)?;
+			list.push(db, value_root)?;
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 /// Partial item.
 pub trait PartialItem {
-    /// Create a new partial item.
-    fn new(index: PartialIndex) -> Self;
+	/// Create a new partial item.
+	fn new(index: PartialIndex) -> Self;
 
-    /// Flush the value back to the database.
-    fn flush<R: RootStatus, DB: WriteBackend>(
-        &mut self,
-        raw: &mut Raw<R, DB::Construct>,
-        db: &mut DB,
-    ) -> Result<(), Error<DB::Error>> where
-        DB::Construct: CompatibleConstruct;
+	/// Flush the value back to the database.
+	fn flush<R: RootStatus, DB: WriteBackend>(
+		&mut self,
+		raw: &mut Raw<R, DB::Construct>,
+		db: &mut DB,
+	) -> Result<(), Error<DB::Error>> where
+		DB::Construct: CompatibleConstruct;
 }
 
 /// Partialable
 pub trait Partialable {
-    /// Value type of the partial item.
-    type Value: PartialItem;
+	/// Value type of the partial item.
+	type Value: PartialItem;
 }
 
 macro_rules! basic_partialables {
-    ( $( $t:ty ),* ) => { $(
-        impl Partialable for $t {
-            type Value = PartialValue<$t>;
-        }
-    )* }
+	( $( $t:ty ),* ) => { $(
+		impl Partialable for $t {
+			type Value = PartialValue<$t>;
+		}
+	)* }
 }
 
 basic_partialables!(u8, u16, u32, u64, u128, U256, H256);
--- src/utils.rs
+++ src/utils.rs
@@ -6,57 +6,57 @@ use generic_array::ArrayLength;
 
 /// Required depth of given length.
 pub fn required_depth(len: u64) -> usize {
-    let mut max_len = 1;
-    let mut total_depth = 0;
-    while max_len < len {
-        max_len *= 2;
-        total_depth += 1;
-    }
-    total_depth
+	let mut max_len = 1;
+	let mut total_depth = 0;
+	while max_len < len {
+		max_len *= 2;
+		total_depth += 1;
+	}
+	total_depth
 }
 
 /// Serialize a vector at given depth.
 pub fn vector_tree<DB: WriteBackend>(values: &[<DB::Construct as Construct>::Value], db: &mut DB, max_len: Option<u64>) -> Result<<DB::Construct as Construct>::Value, Error<DB::Error>> {
-    let total_depth = required_depth(max_len.unwrap_or(values.len() as u64));
+	let total_depth = required_depth(max_len.unwrap_or(values.len() as u64));
 
-    let mut current = values.iter().cloned().collect::<VecDeque<_>>();
-    let mut next = VecDeque::new();
-    for depth in (1..(total_depth + 1)).rev() {
-        let depth_to_bottom = total_depth - depth;
-        while !current.is_empty() {
-            let left = current.pop_front().unwrap_or(<DB::Construct as Construct>::empty_at(db, depth_to_bottom)?);
-            let right = current.pop_front().unwrap_or(<DB::Construct as Construct>::empty_at(db, depth_to_bottom)?);
+	let mut current = values.iter().cloned().collect::<VecDeque<_>>();
+	let mut next = VecDeque::new();
+	for depth in (1..(total_depth + 1)).rev() {
+		let depth_to_bottom = total_depth - depth;
+		while !current.is_empty() {
+			let left = current.pop_front().unwrap_or(<DB::Construct as Construct>::empty_at(db, depth_to_bottom)?);
+			let right = current.pop_front().unwrap_or(<DB::Construct as Construct>::empty_at(db, depth_to_bottom)?);
 
-            let key = <DB::Construct as Construct>::intermediate_of(&left, &right);
+			let key = <DB::Construct as Construct>::intermediate_of(&left, &right);
 
-            db.insert(key.clone(), (left, right))?;
-            next.push_back(key);
-        }
-        current = next;
-        next = VecDeque::new();
-    }
+			db.insert(key.clone(), (left, right))?;
+			next.push_back(key);
+		}
+		current = next;
+		next = VecDeque::new();
+	}
 
-    if current.is_empty() {
-        Ok(<DB::Construct as Construct>::empty_at(db, total_depth)?)
-    } else {
-        Ok(current[0].clone())
-    }
+	if current.is_empty() {
+		Ok(<DB::Construct as Construct>::empty_at(db, total_depth)?)
+	} else {
+		Ok(current[0].clone())
+	}
 }
 
 /// Get the host len of a packed vector.
 pub fn host_max_len<Host: ArrayLength<u8>, Value: ArrayLength<u8>>(value_len: u64) -> u64 {
-    let host_array_len = Host::to_u64();
-    let value_array_len = Value::to_u64();
+	let host_array_len = Host::to_u64();
+	let value_array_len = Value::to_u64();
 
-    let bytes = value_array_len * value_len;
-    if bytes % host_array_len == 0 {
-        bytes / host_array_len
-    } else {
-        bytes / host_array_len + 1
-    }
+	let bytes = value_array_len * value_len;
+	if bytes % host_array_len == 0 {
+		bytes / host_array_len
+	} else {
+		bytes / host_array_len + 1
+	}
 }
 
 /// Get the host len of a packed vector.
 pub fn host_len<Host: ArrayLength<u8>, Value: ArrayLength<u8>>(value_len: usize) -> usize {
-    host_max_len::<Host, Value>(value_len as u64) as usize
+	host_max_len::<Host, Value>(value_len as u64) as usize
 }
